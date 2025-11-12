a8ba4a027c7b0cda9f963e8d1f5c8ba9577d4795
995933447 <34621597+995933447@users.noreply.github.com>
2025-11-12 19:04:09 +0100

    Add files via upload

--- redisproxy/redisproxy.pb.go
+++ redisproxy/redisproxy.pb.go
@@ -7,7 +7,7 @@
 package redisproxy
 
 import (
-	_ "github.com/995933447/mconfigcenter/ext"
+	_ "github.com/995933447/redisproxy/ext"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
